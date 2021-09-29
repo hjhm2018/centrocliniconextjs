1be18f0df12e40b1b18f679b56e62db21bcef521
Henry Herrera <henryjherreram@gmail.com>
2021-09-29 15:55:13 -0700

    adding data

--- pages/especialidades.js
+++ pages/especialidades.js
@@ -1,7 +1,24 @@
 import Head from 'next/head'
+import especialidadesData from '../assets/data/especialidades'
 
+// export const getStaticProps = async () => {
+//     const res = await fetch('http://localhost:3000/assets/data/especialidades.json', {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application / json"
+//         }
+//     });
+
+//     const data = await res.json();
+//     const newData = JSON.parse(data);
+
+//     return {
+//         props: { especialidadesData: newData }
+//     }
+// }
 
 const especialidades = () => {
+
     return (
         <div>
             <Head>
--- pages/especialistas.js
+++ pages/especialistas.js
@@ -1,6 +1,14 @@
 import Head from 'next/head'
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card';
+import especialistasData from '../assets/data/especialistas'
+import especialidadesData from '../assets/data/especialidades'
+
 
 const especialistas = () => {
+
+    const [especialidad, setEspecialidad] = useState('');
+    const [nombre, setNombre] = useState('');
     return (
         <div>
             <Head>
@@ -8,7 +16,45 @@ const especialistas = () => {
                 <meta name="description" content="Pagina de Especialistas" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1>Especialistas</h1>
+            <div className="col-12">
+                <h2 className="text-center bg-info text-white rounded p-2">Especialistas</h2>
+            </div>
+            <div className="col-12 mx-auto d-flex justify-content-center flex-wrap">
+                <div className="col-md-6 col-12 mx-auto p-2">
+                    <input type="text" className="text-center form-control"
+                        placeholder="Filtrar por nombre" onChange={(e) => setNombre(e.target.value.toLowerCase())} />
+                </div>
+                <div className="col-md-6 col-12 p-2">
+                    <select className="form-control" onChange={(e) => setEspecialidad(e.target.value.toLowerCase())}>
+                        <option value="">Filtrar por Especialidad</option>
+                        {especialidadesData.sort(function (a, b) {
+                            return a.nombre.localeCompare(b.nombre);
+                        }).map((especialidad, index) => (
+                            <option key={index} value={especialidad.nombre}>{especialidad.nombre}</option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+            <div className="col-lg-10 mx-auto d-flex justify-content-center flex-wrap">
+                {especialistasData && especialistasData.filter(especialista =>
+
+                    (nombre && !especialista.nombre.toLowerCase().includes(nombre)) ? false : (especialidad && especialista.especialidad.toLowerCase() !== especialidad) ? false : true
+
+                ).sort(function (a, b) {
+                    return a.especialidad.localeCompare(b.especialidad);
+                }).map(((especialista, index) => (
+                    <Card className="text-center m-2" style={{ width: '18rem' }} key={index}>
+                        <Card.Body>
+                            <Card.Title>{especialista.nombre}</Card.Title>
+
+                            <h6>{especialista.especialidad}</h6>
+                            <p><span className="font-weight-bold">Horario: </span>{especialista.horario}</p>
+
+                        </Card.Body>
+                    </Card>
+                )))}
+                {especialistasData.filter(especialista => especialista.nombre.toLowerCase().includes(nombre)).length < 1 && <div className="col-12 p-5 text-center font-weight-bold">No hay resultados</div>}
+            </div>
         </div>
     )
 }
