b3f325b7c14462051840aea7ccadb3b0d4f219da
Henry Herrera <henryjherreram@gmail.com>
2021-09-30 15:41:52 -0700

    adding components

--- pages/especialidades.js
+++ pages/especialidades.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import especialidadesData from '../assets/data/especialidades'
+import React, { useState } from 'react';
+import { Card, Button, Modal } from 'react-bootstrap'
+import Image from 'next/image'
 
 // export const getStaticProps = async () => {
 //     const res = await fetch('http://localhost:3000/assets/data/especialidades.json', {
@@ -19,6 +22,18 @@ import especialidadesData from '../assets/data/especialidades'
 
 const especialidades = () => {
 
+    const [modalContenido, setModalContenido] = useState({ titulo: '', descripcion: '' });
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = (a, b) => {
+        setShow(true);
+        setModalContenido({ titulo: a, descripcion: b });
+    };
+
+    const [especialidadNombre, setEspecialidadNombre] = useState('');
+
     return (
         <div>
             <Head>
@@ -26,7 +41,50 @@ const especialidades = () => {
                 <meta name="description" content="Pagina de Especialidades" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1>Especialidades</h1>
+            <div className="col-12">
+                <h2 className="text-center bg-info text-white rounded p-2">Especialidades</h2>
+            </div>
+            <div className="d-none d-md-block mx-auto col-10 text-center mb-2">
+                {especialidadesData && especialidadesData.sort(function (a, b) {
+                    return a.nombre.localeCompare(b.nombre);
+                }).map((especialidad, index) => (
+                    index % 2 === 0 ?
+                        <span class="badge badge-info m-1" role="button" onClick={() => setEspecialidadNombre(`${especialidad.nombre.toLowerCase()}`)}>#{especialidad.nombre}</span> :
+                        <span class="badge badge-secondary m-1" role="button" onClick={() => setEspecialidadNombre(`${especialidad.nombre.toLowerCase()}`)}>#{especialidad.nombre}</span>
+                ))}
+            </div>
+            <div className="text-center mx-auto col-10">
+                <input type="text" onChange={(e) => setEspecialidadNombre(e.target.value.toLowerCase())} className="rounded text-center"
+                    placeholder="Filtrar por especialidad" />
+            </div>
+            <div className="col-lg-10  mx-auto d-flex justify-content-center flex-wrap">
+                {especialidadesData && especialidadesData.filter(respuesta => respuesta.nombre.toLowerCase().includes(especialidadNombre)).sort(function (a, b) {
+                    return a.nombre.localeCompare(b.nombre);
+                }).map((especialidad, index) => (
+                    <Card style={{ width: '18rem' }} className="text-center m-2" key={index}>
+                        <Card.Img as={Image} variant="top" src={especialidad.imagen} height={200} width={200} />
+                        <Card.Body>
+                            <Card.Title>{especialidad.nombre}</Card.Title>
+                            <Button variant="primary" onClick={() => handleShow(`${especialidad.nombre}`, `${especialidad.servicios}`)}>
+                                Más Info
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                ))}
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title className="ml-auto">{modalContenido.titulo}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="text-center">{modalContenido.descripcion}</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Cerrar
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                {especialidadesData.filter(respuesta => respuesta.nombre.toLowerCase().includes(especialidadNombre)).length < 1 && <div className="col-12 p-5 text-center font-weight-bold">No hay resultados</div>}
+            </div>
         </div>
     )
 }
